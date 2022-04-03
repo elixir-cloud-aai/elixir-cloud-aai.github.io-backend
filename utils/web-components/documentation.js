14483c8eb12ec54441479a6ea36a027458d8614b
Anurag Gupta <anurag@Anurags-MacBook-Air.local>
2022-04-03 22:45:03 +0530

    Docs Backend

--- utils/web-components/documentation.js
+++ utils/web-components/documentation.js
@@ -14,46 +14,68 @@ const getWcDocs = async (query) => {
             },
         }
         var data = await notion.request(payload)
-        console.log(data)
         const productsDBId = data.results[0].id
         payload = {
             path: `blocks/${productsDBId}/children`,
             method: `GET`,
         }
         var { results } = await notion.request(payload)
-        results = results.map((result) => {
+        var finalResults = []
+        for (var i = 0; i < results.length; i++) {
+            var result = results[i]
             if (result.image && result.image.type == 'external') {
-                return {
-                    id: result.id,
-                    type: result.type,
-                    image: result.image.external.url,
-                    createdAt: result.created_time,
-                    updatedAt: result.last_edited_time,
-                }
+                finalResults = [
+                    ...finalResults,
+                    {
+                        id: result.id,
+                        type: result.type,
+                        image: result.image.external.url,
+                        createdAt: result.created_time,
+                        updatedAt: result.last_edited_time,
+                    },
+                ]
+                continue
             }
             if (result.type == 'divider') {
-                return {
+                finalResults = [
+                    ...finalResults,
+                    {
+                        id: result.id,
+                        type: result.type,
+                        createdAt: result.created_time,
+                        updatedAt: result.last_edited_time,
+                    },
+                ]
+                continue
+            }
+            if (result.type == 'table') {
+                // finalResults = [...finalResults, {}]
+                // payload = {
+                //     path: `blocks/${result.id}/children`,
+                //     method: `GET`,
+                // }
+                // var { tableResults } = await notion.request(payload)
+                // console.log(tableResults)
+                continue
+            }
+            finalResults = [
+                ...finalResults,
+                {
                     id: result.id,
                     type: result.type,
+                    text: result[result.type].text.map((block) => {
+                        return {
+                            content: block.plain_text,
+                            link: block.href,
+                            annotations: { ...block.annotations },
+                        }
+                    }),
                     createdAt: result.created_time,
                     updatedAt: result.last_edited_time,
-                }
-            }
-            return {
-                id: result.id,
-                type: result.type,
-                text: result[result.type].text.map((block) => {
-                    return {
-                        content: block.plain_text,
-                        link: block.href,
-                        annotations: { ...block.annotations },
-                    }
-                }),
-                createdAt: result.created_time,
-                updatedAt: result.last_edited_time,
-            }
-        })
-        return results
+                },
+            ]
+        }
+        return finalResults
     } catch (e) {
         console.log(e)
         return { message: 'Server error', error: e }
